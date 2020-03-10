4adc817bdb506715dbdae29fb615d6a0cb421869
senthilvel38782 <senthilbegins86@gmail.com>
2020-03-10 21:02:44 +0530

    text user box completed

--- src/test/java/org/test/senthil/AirSenthill.java
+++ src/test/java/org/test/senthil/AirSenthill.java
@@ -3,6 +3,7 @@ package org.test.senthil;
 public class AirSenthill {
 	public static void main(String[] args) {
 		System.out.println("login coding is completed");
+		System.out.println("textuser box coding completed");
 		
 		
 	}
